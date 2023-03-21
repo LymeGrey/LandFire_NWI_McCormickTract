5fe0f69edbb4fdcdcfd7b94aacac1a9cb97f8329
LymeGrey <127426383+LymeGrey@users.noreply.github.com>
2023-03-21 13:43:12 -0400

    Delete test.R

--- test.R
+++
@@ -1,7 +0,0 @@
-test 
-## install if needed (do this exactly once):
-install.packages("usethis")
-
-library(usethis)
-use_git_config(user.name = "LymeGrey", user.email = "ligray@nmu.edu")
-This is a test 
